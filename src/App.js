6cb3220b31d94838009c72994cf4ceda54f4fbf1
amr303 <amr.abdulmatin@gmail.com>
2022-03-20 14:12:15 +0600

    component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Device from './components/Device/Device';
+import DeviceDetail from './components/DeviceDetail/DeviceDetail';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Matin</h1>
+      <Device name="uphone" price="14000"></Device>
     </div>
   );
 }
