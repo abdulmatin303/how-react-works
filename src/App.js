2d12ff8d502d9e129666535f6326e01702fffd0b
amr303 <amr.abdulmatin@gmail.com>
2022-03-20 15:27:54 +0600

    button added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Device from './components/Device/Device';
 import DeviceDetail from './components/DeviceDetail/DeviceDetail';
+import Watch from './components/Watch/Watch';
 
 function App() {
   return (
     <div className="App">
       <Device name="uphone" price="14000"></Device>
+      <Watch></Watch>
     </div>
   );
 }
